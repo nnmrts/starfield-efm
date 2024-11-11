921f2ed596f1554b801e300df7828cc0e9d4aeed
Nano Miratus <nanomiratus@gmail.com>
2024-11-11 03:42:14 +0100

    adapt bat file

--- scripts/EFM/build.bat
+++ scripts/EFM/build.bat
@@ -1,1 +1,3 @@
 CALL "C:\Program Files (x86)\Steam\steamapps\common\Starfield\Tools\Papyrus Compiler\PapyrusCompiler.exe" "EFM.ppj"
+COPY "C:\Program Files (x86)\Steam\steamapps\common\Starfield\Data\Scripts\EFM.pex" "..\..\dist\scripts"
+COPY "C:\Program Files (x86)\Steam\steamapps\common\Starfield\Data\efm.esp" "..\..\dist"
